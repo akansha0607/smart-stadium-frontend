939dfaa7c469f859a046d3996b0fa48c40d85024
akanshasaxena <akansha.saxbly@gmail.com>
2026-04-26 13:58:16 +0530

    backend url changed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import MapView from "./MapView";
-const BASE_URL = "https://smart-stadium-backend-101294621882.asia-south1.run.app/stadium/crowd";
-
+const BASE_URL = "https://smart-stadium-backend-101294621882.asia-south1.run.app/stadium";
 
 function App() {
   // Crowd
